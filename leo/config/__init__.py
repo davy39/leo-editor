c5da1d4852107fc877b344311e71fbf9a980210b
Davy39 <cottet_davy@yahoo.fr>
2014-12-08 18:04:09 +0800

    First try to upload Leo website

--- leo/config/__init__.py
+++
@@ -1,4 +0,0 @@
-"""leo.config contains
- * the default leoSettings.py
- * an example of custom settings exampleSettings.leo
- * Some example of theme custom settings themes.leo, leoXXptX.leo"""
--- leo/test/unittest/at-auto-line-number-test.py
+++
@@ -1,2 +0,0 @@
-def at_auto_child():
-    pass
